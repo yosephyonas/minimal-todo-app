cb319e938cf7e154f7fac3ab049f25b4d4a29a27
Yosef Yonas <101545038+yosephyonas@users.noreply.github.com>
2023-06-26 11:45:31 +0300

    Update HiveDataBase.dart

--- lib/HiveDataBase.dart
+++ lib/HiveDataBase.dart
@@ -1,20 +1,53 @@
-// ignore: file_names
-import 'package:hive_flutter/hive_flutter.dart';
+// hive_database.dart
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:hive/hive.dart';
+
+import 'task.dart';
 
 class HiveDataBase {
-  List todolist = [];
+  Box<Task>? taskBox;
+  Box<int>? currentIdBox;
 
-  final _mybox = Hive.box('mybox');
+  final FlutterLocalNotificationsPlugin notificationsPlugin;
 
-  void createInitialData() {
-    todolist = [];
+  HiveDataBase(this.notificationsPlugin) {
+    taskBox = Hive.box<Task>('tasks');
+    currentIdBox = Hive.box<int>('currentId');
   }
 
-  void loadDatabase() {
-    todolist = _mybox.get("todolist");
+  List<Task> get taskList => taskBox?.values.toList() ?? [];
+  int get currentId => currentIdBox?.get(0) ?? 0;
+
+  void createInitialData() {
+    currentIdBox?.put(0, 0);
+  }
+
+  void addTask(Task task) {
+    final taskId = currentId + 1;
+
+    final newTask = Task(
+      id: taskId,
+      name: task.name,
+      completed: task.completed,
+      selectedDate: task.selectedDate,
+    );
+
+    taskBox?.put(taskId, newTask);
+    currentIdBox?.put(0, taskId);
+  }
+
+  void removeTask(Task task) {
+    taskBox?.delete(task.id);
+    taskBox?.values.toList();
+  }
+
+  List<Task> loadDatabase() {
+    return taskBox?.values.toList() ?? [];
   }
 
   void updateDataBase() {
-    _mybox.put("todolist", todolist);
+    taskBox?.values.toList().forEach((task) {
+      taskBox?.put(task.id, task);
+    });
   }
 }
