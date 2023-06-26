246cb585b4e05a2fef0ce395e0e21b55d10274e4
Yosef Yonas <101545038+yosephyonas@users.noreply.github.com>
2023-06-26 11:50:14 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
 
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 import 'MAINPAGE.dart';
+import 'NOTIFICATION.dart';
+import 'task.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
-  var _mybox = await Hive.openBox('mybox');
 
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  Hive.registerAdapter(TaskAdapter());
 
-  Future.delayed(
-    const Duration(seconds: 1),
+  await Hive.openBox<Task>('tasks');
+  await Hive.openBox<int>('currentId');
+  await initializeFlutterLocalNotificationsPlugin();
+
+  tz.initializeTimeZones();
+  tz.setLocalLocation(tz.getLocation('Africa/Addis_Ababa'));
+
+  await Future.delayed(const Duration(seconds: 1));
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => SelectedDateTimeProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: const MyApp(),
+    ),
   );
 
-  runApp(const MyApp());
-
   FlutterNativeSplash.remove();
+}
+Future<void> initializeFlutterLocalNotificationsPlugin() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('home');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 }
 
 class MyApp extends StatelessWidget {
