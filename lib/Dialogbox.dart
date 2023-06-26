e773fb7ef530e202be3d7a0382dbbe17600d95a3
Yosef Yonas <101545038+yosephyonas@users.noreply.github.com>
2023-06-26 11:45:01 +0300

    Update Dialogbox.dart

--- lib/Dialogbox.dart
+++ lib/Dialogbox.dart
@@ -1,81 +1,199 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
-import 'package:to_do/button.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:intl/intl.dart';
+import 'package:timezone/timezone.dart' as tz;
 
-class Dialogbox extends StatelessWidget {
+class DialogBox extends StatefulWidget {
   final TextEditingController controller;
-  final VoidCallback onSave;
-  final VoidCallback onCancel;
+  final Function onSave;
+  final Function onCancel;
+  final FlutterLocalNotificationsPlugin notificationsPlugin;
+  final Function refreshTasksCallback;
+  final DateTime selectedDate;
+  final TimeOfDay selectedTime;
+  final int taskId;
 
-  const Dialogbox({
+  const DialogBox({
     Key? key,
     required this.controller,
     required this.onSave,
     required this.onCancel,
+    required this.notificationsPlugin,
+    required this.refreshTasksCallback,
+    required this.selectedDate,
+    required this.selectedTime,
+    required this.taskId,
   }) : super(key: key);
+
+  @override
+  _DialogBoxState createState() => _DialogBoxState();
+}
+
+class _DialogBoxState extends State<DialogBox> {
+  late DateTime selectedDate;
+  late TimeOfDay selectedTime;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = widget.selectedDate;
+    selectedTime = widget.selectedTime;
+  }
+
+  Future<void> _selectDate() async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate; // Update selectedDate when a date is picked
+      });
+    }
+  }
+
+  Future<void> _selectTime() async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.fromDateTime(DateTime.now()),
+    );
+    if (pickedTime != null && pickedTime != selectedTime) {
+      setState(() {
+        selectedTime = pickedTime;
+      });
+    }
+  }
+
+  Future<void> Taskreminder({
+    required String title,
+    required String body,
+    required DateTime scheduledDate,
+    required FlutterLocalNotificationsPlugin fln,
+  }) async {
+    final tz.TZDateTime tzScheduledDate =
+        tz.TZDateTime.from(scheduledDate, tz.local);
+
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await fln.zonedSchedule(
+      widget.taskId,
+      title,
+      body,
+      tzScheduledDate,
+      platformChannelSpecifics,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.white,
-      content: SizedBox(
-        height: 120,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 12,
-                  horizontal: 16,
-                ),
-                hintText: "Add task",
-                hintStyle: const TextStyle(color: Colors.grey),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
-                    width: 1,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Colors.orange,
-                    width: 2,
-                  ),
-                ),
+      contentPadding: EdgeInsets.zero,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+            child: TextField(
+              controller: widget.controller,
+              decoration: const InputDecoration(
+                labelText: 'Task Name',
+                border: OutlineInputBorder(),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
               children: [
-                MyButton(
-                  text: "Save",
-                  textColor: Colors.white,
-                  buttonColor: Colors.orange,
-                  onPressed: onSave,
+                Expanded(
+                  flex: 2,
+                  child: InkWell(
+                    onTap: _selectDate,
+                    child: InputDecorator(
+                      decoration: const InputDecoration(
+                        labelText: 'Date',
+                        border: OutlineInputBorder(),
+                      ),
+                      child: Text(
+                        DateFormat('EEE, MMM d, y').format(
+                          selectedDate,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-                const SizedBox(
-                  width: 8,
-                ),
-                MyButton(
-                  text: "Cancel",
-                  textColor: Colors.orange,
-                  buttonColor: Colors.white,
-                  onPressed: onCancel,
+                const SizedBox(width: 10),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: _selectTime,
+                    child: InputDecorator(
+                      decoration: const InputDecoration(
+                        labelText: 'Time',
+                        border: OutlineInputBorder(),
+                      ),
+                      child: Text(selectedTime.format(context)),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 16.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: widget.onCancel as void Function()?,
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  widget.onSave();
+                  Taskreminder(
+                    title: 'Task Reminder',
+                    body: widget.controller.text,
+                    scheduledDate: DateTime(
+                      selectedDate.year,
+                      selectedDate.month,
+                      selectedDate.day,
+                      selectedTime.hour,
+                      selectedTime.minute,
+                    ),
+                    fln: widget.notificationsPlugin,
+                  );
+                },
+                child: const Text(
+                  'Save',
+                  style: TextStyle(color: Colors.orange),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
